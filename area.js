2b61dda32eba1fa82476ec92a50a008584099039
vargassebas <vargas4744@gmail.com>
2021-10-05 20:06:39 -0600

    corriendo error de eleccion

--- area.js
+++ area.js
@@ -1,50 +1,39 @@
-var circulo, rectangulo, triangulo
+var circulo, rectangulo, triangulo;
 var pi = Math.PI;
 
 do {
+	eleccionu = prompt(
+		'Escoja una de las operaciones:\n1. Circulo\n2. Rectangulo\n3. Triangulo\n4. Salir'
+	).toUpperCase();
 
-eleccionu = prompt("Escoja una de las operaciones: 1. Circulo <br> 2. Rectangulo <br> 3. Triangulo 4. Salir").toUpperCase();
+	if (eleccionu == 1 || eleccionu == 2 || eleccionu == 3 || eleccionu == 4) {
+		if (eleccionu == 1) {
+			radio = prompt('Escriba el radio del circulo: ');
+			circulo = pi * (radio * radio);
+			alert('El area del circulo es: ' + circulo.toFixed(2));
+			document.write('El area del circulo es: ' + circulo.toFixed(2));
+			document.write('<br>');
+		}
 
+		if (eleccionu == 2) {
+			base = prompt('Escriba la base el rectangulo: ');
+			altura = prompt('Escriba la altura del rectangulo: ');
+			rectangulo = base * altura;
+			alert('El area del rectangulo es: ' + rectangulo);
+			document.write('El area del rectangulo es: ' + rectangulo);
+			document.write('<br>');
+		}
 
-if (eleccionu == "CIRCULO" || eleccionu == "RECTANGULO" || eleccionu == "TRIANGULO" || eleccionu == "SALIR"){
-
-if (eleccionu == "CIRCULO"){
-
-    radio = prompt("Escriba el radio del circulo: ");
-    circulo = pi * (radio * radio);
-    alert("El area del circulo es: " + circulo.toFixed(2));
-    document.write("El area del circulo es: " + circulo.toFixed(2));
-    document.write("<br>");
-
-}
-
-if (eleccionu == "RECTANGULO"){
-
-    base = prompt("Escriba la base el rectangulo: ");
-    altura = prompt("Escriba la altura del rectangulo: ");
-    rectangulo = base * altura;
-    alert("El area del rectangulo es: " + rectangulo);
-    document.write("El area del rectangulo es: " + rectangulo);
-    document.write("<br>");
-
-}
-
-if (eleccionu == "TRIANGULO"){
-
-    base = prompt("Escriba la base del triangulo: ");
-    altura = prompt("Escriba la altura del triangulo: ");
-    triangulo = (base * altura)/2;
-    alert("El area del triangulo es: " + triangulo);
-    document.write("El area del triangulo es: " + triangulo);
-    document.write("<br>");
-
-}
-}
-else {
-
-    alert("El dato ingresado no es correcto! Recuerde escribir solo numeros.")
-    location.reload();
-
-}
-
-} while (eleccionu != "SALIR");
+		if (eleccionu == 3) {
+			base = prompt('Escriba la base del triangulo: ');
+			altura = prompt('Escriba la altura del triangulo: ');
+			triangulo = (base * altura) / 2;
+			alert('El area del triangulo es: ' + triangulo);
+			document.write('El area del triangulo es: ' + triangulo);
+			document.write('<br>');
+		}
+	} else {
+		alert('El dato ingresado no es correcto! Recuerde escribir solo numeros.');
+		location.reload();
+	}
+} while (eleccionu != 4);
